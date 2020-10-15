7e71f0a550cd2bba3e7cfe65cedff1294cbe412e
stthoom <stthoom@amazon.com>
2020-10-15 14:16:37 -0400

    Adding below demo VPC changes - Demo VPC CDK construct

--- cli/datamaker_cli/remote_files/cdk/demo.py
+++ cli/datamaker_cli/remote_files/cdk/demo.py
@@ -13,8 +13,9 @@
 #    limitations under the License.
 
 import os
+import json
 import shutil
-from typing import Any, Tuple
+from typing import Any, Tuple, Dict, List
 
 import aws_cdk.aws_ec2 as ec2
 from aws_cdk.core import App, CfnOutput, Construct, Stack, Tags
@@ -24,6 +25,7 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_route53 as r53,
     aws_route53_targets as targets,
+    aws_ssm as ssm,
 )
 
 from datamaker_cli.utils import path_from_filename
@@ -87,24 +89,13 @@ class NewVpcStack(Stack):
             value=",".join(self.isolated_subnets_ids),
         )
 
-
     def _create_vpc(self) -> ec2.Vpc:
-        demo_vpc = ec2.Vpc(
+        demo_vpc = DemoVpc(
             scope=self,
-            id="vpc",
-            default_instance_tenancy=ec2.DefaultInstanceTenancy.DEFAULT,
+            id='vpc',
             cidr="10.191.0.0/16",
-            enable_dns_hostnames=True,
-            enable_dns_support=True,
-            max_azs=2,
-            nat_gateways=1,
             subnet_configuration=[
-                ec2.SubnetConfiguration(name="Isolated", subnet_type=ec2.SubnetType.ISOLATED)
-            ],
-            flow_logs=ec2.FlowLogOptions(
-                destination=ec2.FlowLogDestination.to_cloud_watch_logs(),
-                traffic_type=ec2.FlowLogTrafficType.ALL
-            ),
+                ec2.SubnetConfiguration(name='Isolated', subnet_type=ec2.SubnetType.ISOLATED)],
             vpc_gateway_endpoints={
                 "s3": ec2.GatewayVpcEndpointAwsService.S3,
                 "dynamodb": ec2.GatewayVpcEndpointAwsService.DYNAMODB
@@ -137,21 +128,117 @@ class NewVpcStack(Stack):
             }
         )
 
-        #Adding CA endpoints
+        # Adding CA endpoints
         CodeArtifactEndpoints(self, 'CodeArtifact', demo_vpc.vpc)
 
-        #TODO - Add ServerAccessLogBucket
+        # TODO - Add ServerAccessLogBucket
 
-        Tags.of(scope=demo_vpc).add(key="Env", value=f"datamaker-{self.env_name}")
-        return demo_vpc
+        Tags.of(scope=demo_vpc.vpc).add(key="Env", value=f"datamaker-{self.env_name}")
+        return demo_vpc.vpc
+
+
+DEFAULT_CIDR = "10.191.0.0/16"
+
+
+class DemoVpc(core.Construct):
+
+    def __init__(self, scope: core.Construct, id: str, subnet_configuration: List[ec2.SubnetConfiguration],
+                 cidr=DEFAULT_CIDR, vpc_gateway_endpoints: Dict[str, ec2.IGatewayVpcEndpointService] = {},
+                 vpc_interface_endpoints: Dict[str, ec2.InterfaceVpcEndpointAwsService] = {}, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        self.vpc = ec2.Vpc(scope=self, id="vpc",
+                           cidr=cidr,
+                           subnet_configuration=subnet_configuration,
+                           flow_logs=ec2.FlowLogOptions(
+                               destination=ec2.FlowLogDestination.to_cloud_watch_logs(),
+                               traffic_type=ec2.FlowLogTrafficType.ALL
+                           ),
+                           default_instance_tenancy=ec2.DefaultInstanceTenancy.DEFAULT,
+                           )
+
+        self.interface_service_endpoints = {}
+        self.gateway_service_endpoints = {}
+
+        if self.vpc.public_subnets:
+            self.public_subnets = self.vpc.select_subnets(subnet_type=ec2.SubnetType.PUBLIC)
+        else:
+            self.public_subnets = EmptySubnetSelection()
+
+        if self.vpc.private_subnets:
+            self.private_subnets = self.vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE)
+        else:
+            self.private_subnets = EmptySubnetSelection()
+
+        if self.vpc.isolated_subnets:
+            self.iso_subnets = self.vpc.select_subnets(subnet_type=ec2.SubnetType.ISOLATED)
+        else:
+            self.iso_subnets = EmptySubnetSelection()
+
+        for name, gateway_vpc_endpoint_service in vpc_gateway_endpoints.items():
+            self.add_gateway_endpoint(self.vpc, name, gateway_vpc_endpoint_service,
+                                      self.private_subnets.subnets + self.iso_subnets.subnets)
+
+        for name, interface_service in vpc_interface_endpoints.items():
+            self.add_interface_endpoint(self.vpc, name, interface_service)
+
+        ssm.StringParameter(self, 'VPCConfigParam',
+                            parameter_name=f'/demo/vpc/{id}/config',
+                            string_value=json.dumps(self.to_dict()))
+
+    def to_dict(self):
+        config = {}
+        config['vpcId'] = self.vpc.vpc_id,
+        config['privateSubnets'] = self.private_subnets.subnet_ids,
+        config['publicSubnets'] = self.public_subnets.subnet_ids,
+        config['isolatedSubnets'] = self.iso_subnets.subnet_ids
+        config['defaultSecurityGroup'] = self.vpc.vpc_default_security_group
+        for key, value in self.get_gateway_endpoints().items():
+            if key.endswith('s3'):
+                config['s3GatewayServiceEndpoint'] = value.vpc_endpoint_id
+
+        config['endpointSecurityGroups'] = {}
+
+        for key, value in self.get_interface_endpoints().items():
+            config['endpointSecurityGroups'][value.to_string().split("vpc/", 1)[1]] = [sg.security_group_id for sg in
+                                                                                       value.connections.security_groups]
+        return config
+
+    def get_interface_endpoints(self) -> dict:
+        return self.interface_service_endpoints
+
+    def get_gateway_endpoints(self) -> dict:
+        return self.gateway_service_endpoints
+
+    def add_gateway_endpoint(self, vpc: ec2.Vpc, name: str, service: ec2.GatewayVpcEndpointAwsService, subnets):
+        self.gateway_service_endpoints[service.name] = self.vpc.add_gateway_endpoint(
+            name,
+            service=service,
+            subnets=subnets)
+
+    def add_interface_endpoint(self, vpc: ec2.Vpc, name: str, service: ec2.InterfaceVpcEndpointAwsService):
+        self.interface_service_endpoints[service.name] = self.vpc.add_interface_endpoint(name, service=service)
+
+    def get_gateway_service_endpoint(self, service: ec2.IGatewayVpcEndpointService):
+        return self.gateway_service_endpoints[service.name]
+
+    def get_interface_service_endpoint(self, service: ec2.InterfaceVpcEndpointAwsService):
+        return self.interface_service_endpoints[service.name]
+
+
+class EmptySubnetSelection(ec2.SelectedSubnets):
+    def __init__(self) -> None:
+        super().__init__(availability_zones=[], has_public=False, internet_connectivity_established=None, subnet_ids=[],
+                         subnets=[])
+
 
 class CodeArtifactEndpoints(core.Construct):
     def __init__(self, scope: core.Construct, id: str, vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         vpc.add_interface_endpoint(
-          'code_artifact_api_endpoint',
-          service=ec2.InterfaceVpcEndpointAwsService('codeartifact.api')
+            'code_artifact_api_endpoint',
+            service=ec2.InterfaceVpcEndpointAwsService('codeartifact.api')
         )
 
         code_artifact_repo_endpoint = vpc.add_interface_endpoint(
@@ -172,6 +259,7 @@ class CodeArtifactEndpoints(core.Construct):
             zone=zone,
             target=r53.RecordTarget.from_alias(targets.InterfaceVpcEndpointTarget(code_artifact_repo_endpoint)),
             record_name='*')
+
 
 def synth(stack_name: str, filename: str, env_name: str) -> str:
     filename_dir = path_from_filename(filename=filename)
