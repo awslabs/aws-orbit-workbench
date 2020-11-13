dba5969f8353a81acca0e822cd413ed78c5065a0
Chauncy McCaughey <chamcca@amazon.com>
2020-11-13 14:56:30 -0700

    remove redundant fucntion

--- cli/datamaker_cli/commands/destroy.py
+++ cli/datamaker_cli/commands/destroy.py
@@ -39,33 +39,6 @@ def destroy_toolkit(manifest: Manifest) -> None:
         cfn.destroy_stack(manifest=manifest, stack_name=manifest.toolkit_stack_name)
 
 
-def destroy_image(filename: str, name: str, debug: bool) -> None:
-    with MessagesContext("Destroying Docker Image", debug=debug) as ctx:
-        manifest = Manifest(filename=filename)
-        manifest.fillup()
-        ctx.info(f"Manifest loaded: {filename}")
-        if cfn.does_stack_exist(manifest=manifest, stack_name=f"datamaker-{manifest.name}") is False:
-            ctx.error("Please, deploy your environment before deploy/destroy any docker image")
-            return
-        bundle_path = bundle.generate_bundle(command_name=f"destroy_image-{name}", manifest=manifest, dirs=[])
-        ctx.progress(3)
-        buildspec = codebuild.generate_spec(
-            manifest=manifest,
-            plugins=False,
-            cmds_build=[f"datamaker remote --command destroy_image {name}"],
-        )
-        remote.run(
-            command_name=f"destroy_image-{name}",
-            manifest=manifest,
-            bundle_path=bundle_path,
-            buildspec=buildspec,
-            codebuild_log_callback=ctx.progress_bar_callback,
-            timeout=10,
-        )
-        ctx.info("Docker Image destroyed from ECR")
-        ctx.progress(100)
-
-
 def destroy(filename: str, teams_only: bool, debug: bool) -> None:
     with MessagesContext("Destroying", debug=debug) as ctx:
         manifest = Manifest(filename=filename)
