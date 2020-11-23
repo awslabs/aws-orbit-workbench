73aa95145f7efaf20159fc99d7a05f206bd52381
Chauncy McCaughey <chamcca@amazon.com>
2020-11-23 13:45:07 -0600

    added python repo update scripts for devs

---
+++ cli/update_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+VERSION=$(cat VERSION)
+rm dist/*
+
+aws codeartifact delete-package-versions \
+    --domain aws-datamaker \
+    --repository python-repository \
+    --package datamaker-cli \
+    --versions $VERSION \
+    --format pypi
+
+python setup.py bdist_wheel
+twine upload --repository codeartifact dist/*
