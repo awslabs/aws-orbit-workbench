be724403a5dfe0515af3feb13270877f48104b7c
amirbo <22354730+abaror@users.noreply.github.com>
2020-12-08 18:40:40 -0500

    Working version of a plugin that can run a POD to execute a script for a new Team.  This can be used to do one time tasks or even create Daemons that execute for the team

--- plugins/code_commit/code_commit/__init__.py
+++ plugins/code_commit/code_commit/__init__.py
@@ -36,7 +36,6 @@ def deploy(manifest: Manifest, team_manifest: TeamManifest, parameters: Dict[str
         args=[manifest.filename, team_manifest.name],
     )
 
-
 @hooks.destroy
 def destroy(manifest: Manifest, team_manifest: TeamManifest, parameters: Dict[str, Any]) -> None:
     _logger.debug("Destroying CodeCommit plugin resources for team %s", team_manifest.name)
