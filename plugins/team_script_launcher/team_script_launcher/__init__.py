3fa6361a49229aa144852a68548c161a455ab8aa
AMIR BAR-OR <22354730+abaror@users.noreply.github.com>
2020-12-09 12:31:00 -0500

    Feature/s3 sync plugin (#91)
    * 1. Change build timeout to longer so it doesn't fail on timeout (mainly during destroy)
    2. Always login to codeartifac so we don't need to remember (its very short time)
    3. Add ignore for .env too
    
    * Working version of a plugin that can run a POD to execute a script for a new Team.  This can be used to do one time tasks or even create Daemons that execute for the team
    
    * - always finish with the juvyan user
    - cleanup and reformatting.
    
    * added missing line
    
    * few fixes after code review

---
+++ plugins/team_script_launcher/team_script_launcher/__init__.py
@@ -0,0 +1,99 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License").
+#    You may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import logging
+import os
+from typing import Any, Dict
+
+from datamaker_cli import sh
+from datamaker_cli.manifest import Manifest
+from datamaker_cli.manifest.team import TeamManifest
+from datamaker_cli.plugins import hooks
+from datamaker_cli.remote_files.utils import get_k8s_context
+
+_logger: logging.Logger = logging.getLogger("datamaker_cli")
+PLUGIN_ID = "team-script-launcher"
+CONFIGMAP_SCRIPT_NAME = f"{PLUGIN_ID}-script"
+POD_FILENAME = os.path.join(os.path.dirname(os.path.dirname(__file__)), "job_definition.yaml")
+
+
+@hooks.deploy
+def deploy(manifest: Manifest, team_manifest: TeamManifest, parameters: Dict[str, Any]) -> None:
+    _logger.debug("Team Env name: %s | Team name: %s", manifest.name, team_manifest.name)
+    sh.run(f"echo 'Team Env name: {manifest.name} | Team name: {team_manifest.name}'")
+    # Run the following command to authorize calls to kubectl for the current client
+    sh.run(f"eksctl utils write-kubeconfig --cluster datamaker-{manifest.name} --set-kubeconfig-context")
+
+    if "script" in parameters:
+        script_body = parameters["script"]
+    else:
+        raise Exception(f"Plugin {PLUGIN_ID} must define parameter 'script'")
+    script_file = os.path.join(os.path.dirname(POD_FILENAME), f"{PLUGIN_ID}-script.sh")
+
+    with open(script_file, "w") as file:
+        file.write(script_body)
+
+    # Cleanup of previous installation if needed
+    sh.run(f"kubectl delete jobs/team-script-{PLUGIN_ID} --namespace {team_manifest.name} --ignore-not-found")
+    sh.run(f"kubectl delete configmap {CONFIGMAP_SCRIPT_NAME} --namespace {team_manifest.name} --ignore-not-found")
+
+    # Create the configmap with the script
+    sh.run(
+        f"kubectl create configmap {CONFIGMAP_SCRIPT_NAME} --from-file={script_file} --namespace {team_manifest.name}"
+    )
+    _logger.debug(f"Create config map: {CONFIGMAP_SCRIPT_NAME} at namespace {team_manifest.name}")
+
+    context = get_k8s_context(manifest=manifest)
+    _logger.debug(
+        "Using S3 Sync Pod at %s for Env name: %s | Team name: %s, CONTEXT: %s",
+        POD_FILENAME,
+        manifest.name,
+        team_manifest.name,
+        context,
+    )
+    input = POD_FILENAME
+    output = os.path.join(os.path.dirname(POD_FILENAME), f"{PLUGIN_ID}-team.yaml")
+
+    with open(input, "r") as file:
+        content: str = file.read()
+
+    restart_policy = parameters["restartPolicy"] if "restartPolicy" in parameters else "Never"
+    content = content.replace("$", "").format(
+        team=team_manifest.name,
+        region=manifest.region,
+        account_id=manifest.account_id,
+        env_name=manifest.name,
+        tag=team_manifest.manifest.images["jupyter-hub"]["version"],
+        restart_policy=restart_policy,
+        plugin_id=PLUGIN_ID,
+    )
+
+    _logger.debug("Kubectl Team %s manifest:\n%s", team_manifest.name, content)
+    with open(output, "w") as file:
+        file.write(content)
+
+    # run the POD to execute the script
+    cmd = f"kubectl apply -f {output} --context {context} --namespace {team_manifest.name}"
+    _logger.debug(cmd)
+    sh.run(cmd)
+
+
+@hooks.destroy
+def destroy(manifest: Manifest, team_manifest: TeamManifest, parameters: Dict[str, Any]) -> None:
+    _logger.debug("Delete Plugin %s of Team Env name: %s | Team name: %s", PLUGIN_ID, manifest.name, team_manifest.name)
+    sh.run(f"echo 'Team Env name: {manifest.name} | Team name: {team_manifest.name}'")
+    sh.run(f"eksctl utils write-kubeconfig --cluster datamaker-{manifest.name} --set-kubeconfig-context")
+
+    sh.run(f"kubectl delete jobs/team-script-{PLUGIN_ID} --namespace {team_manifest.name} --ignore-not-found")
+    sh.run(f"kubectl delete configmap {CONFIGMAP_SCRIPT_NAME} --namespace {team_manifest.name} --ignore-not-found")
