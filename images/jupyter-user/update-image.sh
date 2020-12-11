cd44a86b8c0ca6e8c7fdab53e45ddcbdfcd1ab42
Chauncy McCaughey <chamcca@amazon.com>
2020-12-11 15:13:58 -0600

    remove local update file

--- images/jupyter-user/update-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-docker build --tag aws-datamaker-jupyter-user:latest .
-docker tag aws-datamaker-jupyter-user:latest 011316101677.dkr.ecr.us-west-2.amazonaws.com/datamaker-dev-env-jupyter-user:latest
-docker push 011316101677.dkr.ecr.us-west-2.amazonaws.com/datamaker-dev-env-jupyter-user:latest
-
