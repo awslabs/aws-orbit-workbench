6464600deb641840c845c8c76e0c3e6610013dd1
Chauncy McCaughey <chamcca@amazon.com>
2021-01-28 18:34:13 -0700

    updated developer scripts

--- scripts/prep_build_from_source.sh
+++ scripts/prep_build_from_source.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/vars
 
-$DIR/set_jupyter_user_pip_conf.sh
-$DIR/update_repo.sh cli aws-orbit
-$DIR/update_repo.sh sdk aws-orbit-sdk
+${DIR}/set_jupyter_user_pip_conf.sh \
+    && echo "Set jupyter-user pip.conf" \
+    || (echo "ERROR: Failed to set jupyter-user pip.conf"; exit 1)
+${DIR}/update_repo.sh cli ${DOMAIN} \
+    && echo "Updated CLI codeartifact repository" \
+    || (echo "ERROR: Failed to update CLI codeartifact repository"; exit 1)
+${DIR}/update_repo.sh sdk ${DOMAIN}-sdk \
+    && echo "Updated SDK codeartifact repository" \
+    || (echo "ERROR: Failed to update SDK codeartifact repository"; exit 1)
--- scripts/set_jupyter_user_pip_conf.sh
+++ scripts/set_jupyter_user_pip_conf.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/vars
 
-aws codeartifact login --tool pip --domain aws-orbit --repository python-repository
-cp ~/.config/pip/pip.conf $DIR/../images/jupyter-user/
+aws codeartifact login --tool pip --domain ${DOMAIN} --repository ${REPOSITORY} \
+    && echo "Logged in to codeartifact domain/repository: ${DOMAIN}/${REPOSITORY}" \
+    || (echo "ERROR: Failed to login to codeartifact domain/repository: ${DOMAIN}/${REPOSITORY}"; exit 1)
+cp ~/.config/pip/pip.conf ${IMAGES_DIR}/jupyter-user/ \
+    && echo "Copied ~/.config/pip/pip.conf to ${IMAGES_DIR}/jupyter-user/" \
+    || (echo "ERROR: Failed to copy ~/.config/pip/pip.conf to ${IMAGES_DIR}/jupyter-user/"; exit 1)
