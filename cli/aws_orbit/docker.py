f70845c949313f7ef3760cce7e3fcc370a534bb4
Ramon Bautista <bautramo@amazon.com>
2021-02-03 21:55:46 -0500

    Check to see if tagging image is from a public ecr

--- cli/aws_orbit/docker.py
+++ cli/aws_orbit/docker.py
@@ -62,7 +62,7 @@ def ecr_pull_external(manifest: "Manifest", repository: str, tag: str = "latest"
 
 def tag_image(manifest: "Manifest", remote_name: str, remote_source: str, name: str, tag: str = "latest") -> None:
     ecr_address = f"{manifest.account_id}.dkr.ecr.{manifest.region}.amazonaws.com"
-    if remote_source == "ecr":
+    if remote_source == "ecr" and not remote_name.startswith("public.ecr.aws"):
         remote_name = f"{ecr_address}/{remote_name}"
     sh.run(f"docker tag {remote_name}:{tag} {ecr_address}/{name}:{tag}")
 
