657b111b87a52ca733bc9b63f5592c4065259ba0
amirbo <22354730+abaror@users.noreply.github.com>
2021-02-09 16:17:50 -0600

    - Restart jupyterhub to allow new profiles to appear in the jupyterhub menu. - Fix list command to show only once the env.

--- cli/aws_orbit/remote_files/utils.py
+++ cli/aws_orbit/remote_files/utils.py
@@ -1,6 +1,9 @@
+import logging
 from typing import TYPE_CHECKING, List
 
 from kubernetes import config
+
+_logger: logging.Logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from aws_orbit.manifest import Manifest
@@ -9,8 +12,9 @@ if TYPE_CHECKING:
 def get_k8s_context(manifest: "Manifest") -> str:
     try:
         contexts: List[str] = [str(c["name"]) for c in config.list_kube_config_contexts()[0]]
-    except config.config_exception.ConfigException:
-        raise RuntimeError("Context not found")
+    except config.config_exception.ConfigException as e:
+        _logger.debug("Context not found")
+        raise e
     expected_domain: str = f"@orbit-{manifest.name}.{manifest.region}.eksctl.io"
     for context in contexts:
         if context.endswith(expected_domain):
