b12842086845951cf79866dfc4dad8c682dd0312
Igor Tavares <igorborgest@gmail.com>
2021-03-11 16:43:38 -0300

    Add the first jupyterlab-extension prototype.
    update.
    
    update.
    
    update.
    
    update.
    
    update.
    
    update.

---
+++ images/jupyter-user/extensions/jupyterlab_orbit/src/team.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { JupyterFrontEnd } from '@jupyterlab/application';
+import { ILauncher } from '@jupyterlab/launcher';
+import { ReactWidget, ICommandPalette } from '@jupyterlab/apputils';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { Menu } from '@lumino/widgets';
+
+import { teamIcon } from './common/icons';
+import { RUNNING_CLASS, SECTION_CLASS } from './common/styles';
+import { CentralWidgetHeader } from './common/headers/centralWidgetHeader';
+import { LeftWidgetHeader } from './common/headers/leftWidgetHeader';
+import { registerLaunchCommand, registerGeneral } from './common/activation';
+
+const NAME = 'Team';
+const ICON: LabIcon = teamIcon;
+
+const refreshCallback = () => {
+  console.log(`[${NAME}] Refresh!`);
+};
+
+class CentralWidget extends ReactWidget {
+  constructor() {
+    super();
+    this.addClass('jp-ReactWidget');
+    this.addClass(RUNNING_CLASS);
+    this.title.caption = `AWS Orbit Workbench - ${NAME}`;
+    this.title.label = `Orbit - ${NAME}`;
+    this.title.icon = ICON;
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className={SECTION_CLASS}>
+        <CentralWidgetHeader
+          name={NAME}
+          icon={ICON}
+          refreshCallback={refreshCallback}
+        />
+        <div />
+      </div>
+    );
+  }
+}
+
+class LeftWidget extends ReactWidget {
+  launchCallback: () => void;
+
+  constructor({ openCallback }: { openCallback: () => void }) {
+    super();
+    this.addClass('jp-ReactWidget');
+    this.addClass(RUNNING_CLASS);
+    this.title.caption = `AWS Orbit Workbench - ${NAME}`;
+    this.title.icon = ICON;
+    this.launchCallback = openCallback;
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className={SECTION_CLASS}>
+        <LeftWidgetHeader
+          name={NAME}
+          icon={ICON}
+          refreshCallback={refreshCallback}
+          openCallback={this.launchCallback}
+        />
+        <div />
+      </div>
+    );
+  }
+}
+
+export const activateTeam = (
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  launcher: ILauncher | null,
+  menu: Menu,
+  rank: number
+) => {
+  const { commands } = app;
+
+  const launchCommand: string = registerLaunchCommand({
+    name: NAME,
+    icon: ICON,
+    app: app,
+    widgetCreation: () => new CentralWidget()
+  });
+
+  registerGeneral({
+    app: app,
+    palette: palette,
+    launcher: launcher,
+    menu: menu,
+    rank: rank,
+    launchCommand: launchCommand,
+    leftWidget: new LeftWidget({
+      openCallback: () => {
+        commands.execute(launchCommand);
+      }
+    })
+  });
+};
