8b1e89907effb6479ce9c12e1d70cac79a6d23fb
abaror <22354730+abaror@users.noreply.github.com>
2021-03-24 08:58:01 -0400

    formatting

--- images/jupyter-user/scripts/mock_exports.sh
+++ images/jupyter-user/scripts/mock_exports.sh
@@ -2,6 +2,6 @@
 
 export MOCK=1
 export AWS_ORBIT_ENV=dev-env
-export AWS_ORBIT_TEAM_SPACE=lake-creator
-export AWS_DEFAULT_REGION=us-east-2
-export JUPYTERHUB_USER=john-doe
+export AWS_ORBIT_TEAM_SPACE=lake-user
+export AWS_DEFAULT_REGION=us-west-2
+export JUPYTERHUB_USER=amirbo
