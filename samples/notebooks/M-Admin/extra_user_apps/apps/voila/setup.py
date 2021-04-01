67df7c0a61bc4a12b3a5f93b926bf917943f7a75
abaror <22354730+abaror@users.noreply.github.com>
2021-04-01 17:34:37 -0400

    adding volia for developers

---
+++ samples/notebooks/M-Admin/extra_user_apps/apps/voila/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name="code-server-proxy",
+  # py_modules rather than packages, since we only have 1 file
+  py_modules=['voila'],
+  entry_points={
+      'jupyter_serverproxy_servers': [
+          # name = packagename:function_name
+          'code-server = voila:setup_voila',
+      ]
+  },
+  install_requires=['jupyter-server-proxy'],
+)
