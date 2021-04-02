c193c1066dc978ea1ccf3cd7209943b73fe55394
abaror <22354730+abaror@users.noreply.github.com>
2021-04-02 08:20:48 -0400

    fixing package names

--- samples/notebooks/M-Admin/extra_user_apps/apps/codeserverproxy/setup.py
+++ samples/notebooks/M-Admin/extra_user_apps/apps/codeserverproxy/setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-  name="voila_orbit_proxy",
+  name="codeserverproxy",
   # py_modules rather than packages, since we only have 1 file
-  py_modules=['voila'],
+  py_modules=['codeserver'],
   entry_points={
       'jupyter_serverproxy_servers': [
           # name = packagename:function_name
-          'code-server = voila:setup_voila',
+          'code-server = codeserverproxy:setup_codeserver',
       ]
   },
-  install_requires=['jupyter-server-proxy~=3.0.2', 'voila==0.2.7', 'bqplot~=0.12.23', 'ipyvuetify~=1.6.2']
+  install_requires=['jupyter-server-proxy~=3.0.2'],
 )
--- samples/notebooks/M-Admin/extra_user_apps/apps/voilaproxy/setup.py
+++ samples/notebooks/M-Admin/extra_user_apps/apps/voilaproxy/setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-  name="codeserver_orbit_proxy",
+  name="voilaproxy",
   # py_modules rather than packages, since we only have 1 file
-  py_modules=['codeserver'],
+  py_modules=['voila'],
   entry_points={
       'jupyter_serverproxy_servers': [
           # name = packagename:function_name
-          'code-server = codeserver:setup_codeserver',
+          'code-server = voilaproxy:setup_voila',
       ]
   },
-  install_requires=['jupyter-server-proxy~=3.0.2'],
+  install_requires=['jupyter-server-proxy~=3.0.2', 'voila==0.2.7', 'bqplot~=0.12.23', 'ipyvuetify~=1.6.2']
 )
