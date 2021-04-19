d1980d8eaeffc0f3a30f8af812c6dd55a1bc9dfa
AMIR BAR-OR <22354730+abaror@users.noreply.github.com>
2021-04-19 15:13:01 -0400

    Helm plugins (#401)
    * fix resources limit
    
    * fsx first commit
    
    * fsx working on public env
    
    * fix req paths
    
    * regression testing
    
    * all working now
    
    * final fixes - test works
    
    * moving the iam change to eksctl
    
    * test but for now disabled until we get the isolated image puller
    
    * imports
    
    * imports
    
    * fixes
    
    * team script plugin now turned into helm chart
    
    * FSX works with helm
    
    * more fixes
    
    * more fixes
    
    * add to lake-user too
    
    * adding longer time build

--- plugins/lustre/lustre/__init__.py
+++ plugins/lustre/lustre/__init__.py
@@ -14,15 +14,18 @@
 
 import logging
 import os
-from typing import TYPE_CHECKING, Any, Dict, Optional,cast
-from aws_orbit import sh, utils
+from typing import TYPE_CHECKING, Any, Dict, Optional, cast
+
+from aws_orbit import utils
 from aws_orbit.plugins import hooks
+from aws_orbit.remote_files import helm
 from aws_orbit.services import ec2
 from aws_orbit.services.ec2 import IpPermission, UserIdGroupPair
+
 if TYPE_CHECKING:
     from aws_orbit.models.context import Context, TeamContext
 _logger: logging.Logger = logging.getLogger("aws_orbit")
-POD_FILENAME = os.path.join(os.path.dirname(__file__), "lustre.yaml")
+CHARTS_PATH = os.path.join(os.path.dirname(__file__), "charts")
 
 
 @hooks.deploy
@@ -36,42 +39,54 @@ def deploy(plugin_id: str, context: "Context", team_context: "TeamContext", para
         account_id=context.account_id,
         env_name=context.name,
         plugin_id=plugin_id,
-        deploymentType='SCRATCH_2',
+        deploymentType="SCRATCH_2",
         sg=team_context.team_security_group_id,
         subnet=context.networking.data.nodes_subnets[0],
-        s3importpath=f's3://{team_context.scratch_bucket}/{team_context.name}/lustre',
-        s3exportpath=f's3://{team_context.scratch_bucket}/{team_context.name}/lustre'
+        s3importpath=f"s3://{team_context.scratch_bucket}/{team_context.name}/lustre",
+        s3exportpath=f"s3://{team_context.scratch_bucket}/{team_context.name}/lustre",
     )
-
-    input = POD_FILENAME
-    output = os.path.join(os.path.dirname(POD_FILENAME), f"{plugin_id}-team.yaml")
-
-    with open(input, "r") as file:
-        content: str = file.read()
-
-    content = utils.resolve_parameters(content, vars)
-
-    _logger.debug("Kubectl Team %s context:\n%s", team_context.name, content)
-    with open(output, "w") as file:
-        file.write(content)
 
     ec2.authorize_security_group_ingress(
         group_id=cast(str, team_context.team_security_group_id),
-        ip_permissions=[IpPermission(
+        ip_permissions=[
+            IpPermission(
                 from_port=988,
                 to_port=988,
                 ip_protocol="tcp",
-                user_id_group_pairs=[UserIdGroupPair(description="All from Cluster", group_id=cast(str, context.cluster_sg_id))],
-            )]
+                user_id_group_pairs=[
+                    UserIdGroupPair(description="All from Cluster", group_id=cast(str, context.cluster_sg_id))
+                ],
+            )
+        ],
     )
 
-    # run the POD to execute the script
-    cmd = f"kubectl apply -f {output}  --namespace {team_context.name}"
-    _logger.debug(cmd)
-    sh.run(cmd)
+    chart_path = helm.create_team_charts_copy(team_context=team_context, path=CHARTS_PATH)
+    _logger.debug("package dir")
+    utils.print_dir(CHARTS_PATH)
+    _logger.debug("copy chart dir")
+    utils.print_dir(chart_path)
+
+    repo_location = helm.init_team_repo(context=context, team_context=team_context)
+    repo = team_context.name
+    helm.add_repo(repo=repo, repo_location=repo_location)
+    chart_name, chart_version, chart_package = helm.package_chart(
+        repo=repo, chart_path=os.path.join(chart_path, "fsx_storageclass"), values=vars
+    )
+    helm.install_chart(
+        repo=repo,
+        namespace=team_context.name,
+        name=f"{team_context.name}-{plugin_id}",
+        chart_name=chart_name,
+        chart_version=chart_version,
+    )
+
+    chart_name, chart_version, chart_package = helm.package_chart(
+        repo=repo, chart_path=os.path.join(chart_path, "fsx_filesystem"), values=vars
+    )
+    _logger.info(f"Lustre Helm Chart {chart_name}@{chart_version} installed for {team_context.name} at {chart_package}")
 
 
 @hooks.destroy
 def destroy(plugin_id: str, context: "Context", team_context: "TeamContext", parameters: Dict[str, Any]) -> None:
     _logger.debug("Delete Plugin %s of Team Env name: %s | Team name: %s", plugin_id, context.name, team_context.name)
-
+    helm.uninstall_chart(f"{team_context.name}-{plugin_id}")
