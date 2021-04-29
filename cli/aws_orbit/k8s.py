6ef42333b920c7d34e66e81540430a45080c7ca2
chamcca <40579012+chamcca@users.noreply.github.com>
2021-04-29 09:46:55 -0600

    drop k8s logging because of serialization failures  (#438)
    * add k8s response logging; update defaults in sample manifest
    
    * add datetime serializer
    
    * type annotations
    
    * drop k8s logging because of serialization failures

--- cli/aws_orbit/k8s.py
+++ cli/aws_orbit/k8s.py
@@ -1,7 +1,5 @@
-import json
 import logging
 import time
-from datetime import date, datetime
 from typing import Any, Dict, cast
 
 from kubernetes import config
@@ -10,20 +8,11 @@ from kubernetes.client import CoreV1Api, V1Service
 _logger: logging.Logger = logging.getLogger(__name__)
 
 
-def json_serial(obj: Any) -> str:
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError("Type %s not serializable" % type(obj))
-
-
 def get_service_hostname(name: str, k8s_context: str, namespace: str = "default") -> str:
     config.load_kube_config(context=k8s_context)
     v1 = CoreV1Api()
     while True:
         resp = cast(V1Service, v1.read_namespaced_service_status(name=name, namespace=namespace))
-        _logger.debug("K8s Response:\n", json.dumps(resp, default=json_serial))
         status: Dict[str, Any] = resp.status.to_dict()
         if "load_balancer" in status:
             if "ingress" in status["load_balancer"]:
