4d389c9c527957ebab3b900546ddd36f4c5469dd
Chauncy McCaughey <chamcca@amazon.com>
2021-04-29 08:42:20 -0600

    add datetime serializer

--- cli/aws_orbit/k8s.py
+++ cli/aws_orbit/k8s.py
@@ -1,5 +1,8 @@
+import json
 import logging
 import time
+
+from datetime import date, datetime
 from typing import Any, Dict, cast
 
 from kubernetes import config
@@ -8,12 +11,20 @@ from kubernetes.client import CoreV1Api, V1Service
 _logger: logging.Logger = logging.getLogger(__name__)
 
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+
 def get_service_hostname(name: str, k8s_context: str, namespace: str = "default") -> str:
     config.load_kube_config(context=k8s_context)
     v1 = CoreV1Api()
     while True:
         resp = cast(V1Service, v1.read_namespaced_service_status(name=name, namespace=namespace))
-        _logger.debug("K8s Response:\n", resp)
+        _logger.debug("K8s Response:\n", json.dumps(resp, default=json_serial))
         status: Dict[str, Any] = resp.status.to_dict()
         if "load_balancer" in status:
             if "ingress" in status["load_balancer"]:
