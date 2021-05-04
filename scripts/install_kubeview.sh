0085b4ac541c11d243bc4377f82e6898fbcc9726
Derek Graeber <dgraeber@amazon.com>
2021-05-04 17:04:53 -0400

    Fix/appsimagetest 0.16.0 (#449)
    * removing hardcoded env params
    
    * Changed demo-foundation to demo-fndn (#365)
    
    * Changed demo-foundation to demo-fndn
    
    * Changed demo-foundation to demo-fndn
    
    * env var for lake-creator and lake-user resources
    
    * Feature/delete leftover redshift clusters (#364)
    
    Refactored orbit admin role. Deleting team specific redshift clusters before destroying redshift plugin.
    
    * Jupyterhub voila service (#366)
    
    * add missing permission
    
    * fixing the broken plugin delete
    
    * validation
    
    * formatting
    
    * first round working codebuild
    
    * second round building from notebook
    
    * adding volia for developers
    
    * code-builder works in the new setup
    
    * fixes
    
    * fixing package names
    
    * desktop, voila, and codebuilder work on my private.  Will try to test now automation
    
    * formatting
    
    * allow storage class access
    
    * remove the update
    
    * remove the update
    
    * fixing names to be different than development env
    
    * fix validations
    
    * fix validations
    
    * better fix for jupyterhub validations
    
    * again formatting
    
    * validation 3
    
    * validation 4
    
    * push code changes
    
    * Tag the version of the image from context version (#367)
    
    * Tag the version of the image from context version
    
    * docker image format typo
    
    * minor edit
    
    * correcting user images
    
    Co-authored-by: AMIR BAR-OR <22354730+abaror@users.noreply.github.com>
    Co-authored-by: Ramon Bautista <36557237+rb201@users.noreply.github.com>
    Co-authored-by: Sreeram <70175917+stthoom@users.noreply.github.com>

--- scripts/install_kubeview.sh
+++ scripts/install_kubeview.sh
@@ -9,7 +9,7 @@ fi
 
 orbit replicate image -e ${ORBIT_ENV_NAME} --source_registry dockerhub --source_repository hjacobs/kube-ops-view  --source_version 20.4.0 --name kube-ops-view
 helm install stable/kube-ops-view --version 1.2.1 --generate-name \
---set image.repository=495869084367.dkr.ecr.us-west-2.amazonaws.com/orbit-dev-env-users-kube-ops-view \
+--set image.repository=495869084367.dkr.ecr.us-west-2.amazonaws.com/orbit-dev-env/users/kube-ops-view \
 --set service.type=LoadBalancer \
 --set nodeSelector.orbit/node-group=env \
 --set nodeSelector.orbit/usage=reserved \
