39e9ce80d814209163eb1d5dfc52098d00ae26f8
Ramon Bautista <bautramo@amazon.com>
2021-05-04 15:06:36 -0400

    Formatting

--- cli/aws_orbit/remote_files/cdk/lambda_sources/cognito_post_authentication/index.py
+++ cli/aws_orbit/remote_files/cdk/lambda_sources/cognito_post_authentication/index.py
@@ -5,6 +5,7 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def handler(event: Dict[str, Any], context: Optional[Dict[str, Any]]) -> Any:
     cognito_client = boto3.client("cognito-idp")
 
@@ -12,7 +13,7 @@ def handler(event: Dict[str, Any], context: Optional[Dict[str, Any]]) -> Any:
     userPoolId = event.get("userPoolId")
 
     userGroupsInfo = cognito_client.admin_list_groups_for_user(Username=userName, UserPoolId=userPoolId)
-    userGroups = [group.get('GroupName') for group in userGroupsInfo.get("Groups")]
+    userGroups = [group.get("GroupName") for group in userGroupsInfo.get("Groups")]
 
     logger.info("Authenticated successful")
     logger.info(userName, userPoolId, userGroups)
