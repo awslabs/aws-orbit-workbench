6a4ca1ed3ca14078bbcc7de0a2c526894b77a19a
Chauncy McCaughey <chamcca@amazon.com>
2021-05-06 17:04:58 -0600

    refactor kubectl manifests

--- cli/aws_orbit/remote_files/kubectl.py
+++ cli/aws_orbit/remote_files/kubectl.py
@@ -30,9 +30,9 @@ _logger: logging.Logger = logging.getLogger(__name__)
 MODELS_PATH = os.path.join(ORBIT_CLI_ROOT, "data", "kubectl")
 
 
-def _commons(context: "Context", output_path: str) -> None:
-    filename = "00-commons.yaml"
-    input = os.path.join(MODELS_PATH, "apps", filename)
+def _orbit_system_commons(context: "Context", output_path: str) -> None:
+    filename = "commons.yaml"
+    input = os.path.join(MODELS_PATH, "orbit-system", filename)
     output = os.path.join(output_path, filename)
 
     with open(input, "r") as file:
@@ -49,35 +49,9 @@ def _commons(context: "Context", output_path: str) -> None:
         file.write(content)
 
 
-def _k8_dashboard(context: "Context", output_path: str) -> None:
-    filename = "05-dashboard.yaml"
-    input = os.path.join(MODELS_PATH, "apps", filename)
-    output = os.path.join(output_path, filename)
-    with open(input, "r") as file:
-        content: str = file.read()
-    content = utils.resolve_parameters(
-        content, dict(image_pull_policy="Always" if aws_orbit.__version__.endswith(".dev0") else "InNotPresent")
-    )
-    with open(output, "w") as file:
-        file.write(content)
-
-
-def _metrics_server(context: "Context", output_path: str) -> None:
-    filename = "06-metrics-server.yaml"
-    input = os.path.join(MODELS_PATH, "apps", filename)
-    output = os.path.join(output_path, filename)
-    with open(input, "r") as file:
-        content: str = file.read()
-    content = utils.resolve_parameters(
-        content, dict(image_pull_policy="Always" if aws_orbit.__version__.endswith(".dev0") else "InNotPresent")
-    )
-    with open(output, "w") as file:
-        file.write(content)
-
-
 def _cluster_autoscaler(output_path: str, context: "Context") -> None:
-    filename = "07-cluster-autoscaler-autodiscover.yaml"
-    input = os.path.join(MODELS_PATH, "apps", filename)
+    filename = "cluster-autoscaler-autodiscover.yaml"
+    input = os.path.join(MODELS_PATH, "kube-system", filename)
     output = os.path.join(output_path, filename)
 
     with open(input, "r") as file:
@@ -98,15 +72,15 @@ def _cluster_autoscaler(output_path: str, context: "Context") -> None:
 
 
 def _ssm_agent_installer(output_path: str, context: "Context") -> None:
-    filename = "09-ssm-agent-daemonset-installer.yaml"
-    input = os.path.join(MODELS_PATH, "apps", filename)
+    filename = "ssm-agent-daemonset-installer.yaml"
+    input = os.path.join(MODELS_PATH, "kube-system", filename)
     output = os.path.join(output_path, filename)
     shutil.copyfile(src=input, dst=output)
 
 
 def _team(context: "Context", team_context: "TeamContext", output_path: str) -> None:
-    input = os.path.join(MODELS_PATH, "apps", "01-team.yaml")
-    output = os.path.join(output_path, f"01-{team_context.name}-team.yaml")
+    input = os.path.join(MODELS_PATH, "teams", "team.yaml")
+    output = os.path.join(output_path, f"{team_context.name}-team.yaml")
 
     with open(input, "r") as file:
         content: str = file.read()
@@ -129,8 +103,8 @@ def _team(context: "Context", team_context: "TeamContext", output_path: str) -> 
         file.write(content)
 
     # user service account
-    input = os.path.join(MODELS_PATH, "apps", "02-user-service-account.yaml")
-    output = os.path.join(output_path, f"02-{team_context.name}-user-service-account.yaml")
+    input = os.path.join(MODELS_PATH, "teams", "user-service-account.yaml")
+    output = os.path.join(output_path, f"{team_context.name}-user-service-account.yaml")
 
     with open(input, "r") as file:
         content = file.read()
@@ -139,8 +113,8 @@ def _team(context: "Context", team_context: "TeamContext", output_path: str) -> 
         file.write(content)
 
     # deploy voila service
-    input = os.path.join(MODELS_PATH, "apps", "08-voila_service.yaml")
-    output = os.path.join(output_path, f"08-{team_context.name}-voila_service.yaml")
+    input = os.path.join(MODELS_PATH, "teams", "voila_service.yaml")
+    output = os.path.join(output_path, f"{team_context.name}-voila_service.yaml")
 
     with open(input, "r") as file:
         content = file.read()
@@ -161,8 +135,8 @@ def _team(context: "Context", team_context: "TeamContext", output_path: str) -> 
     # bind to admin role
     if team_context.k8_admin:
         # user service account
-        input = os.path.join(MODELS_PATH, "apps", "04-admin-binding.yaml")
-        output = os.path.join(output_path, f"04-{team_context.name}-admin-binding.yaml")
+        input = os.path.join(MODELS_PATH, "teams", "admin-binding.yaml")
+        output = os.path.join(output_path, f"{team_context.name}-admin-binding.yaml")
 
         with open(input, "r") as file:
             content = file.read()
@@ -178,12 +152,50 @@ def _cleanup_output(output_path: str) -> None:
             os.remove(os.path.join(output_path, file))
 
 
-def _generate_env_manifest(context: "Context", clean_up: bool = True) -> str:
-    output_path = os.path.join(".orbit.out", context.name, "kubectl", "apps")
+def _generate_kube_system_manifest(context: "Context", clean_up: bool = True) -> str:
+    output_path = os.path.join(".orbit.out", context.name, "kubectl", "kube-system")
     os.makedirs(output_path, exist_ok=True)
     if clean_up:
         _cleanup_output(output_path=output_path)
-    _commons(context=context, output_path=output_path)
+
+    _generate_efs_driver_manifest(context=context)
+    _generate_fsx_driver_manifest(context=context)
+
+    filenames = [
+        "aws-vgpu-daemonset.yaml",
+        "cluster-autoscaler-autodiscover.yaml",
+        "nvidia-daemonset.yaml",
+        "observability.yaml",
+    ]
+    for filename in filenames:
+        input = os.path.join(MODELS_PATH, "kube-system", filename)
+        output = os.path.join(output_path, filename)
+
+        with open(input, "r") as file:
+            content: str = file.read()
+        content = utils.resolve_parameters(
+            content,
+            dict(
+                account_id=context.account_id,
+                env_name=context.name,
+                cluster_name=f"orbit-{context.name}",
+                sts_ep="legacy" if context.networking.data.internet_accessible else "regional",
+                image_pull_policy="Always" if aws_orbit.__version__.endswith(".dev0") else "InNotPresent",
+                use_static_instance_list=str(not context.networking.data.internet_accessible).lower(),
+            ),
+        )
+        with open(output, "w") as file:
+            file.write(content)
+
+    return output_path
+
+
+def _generate_orbit_system_manifest(context: "Context", clean_up: bool = True) -> str:
+    output_path = os.path.join(".orbit.out", context.name, "kubectl", "orbit-system")
+    os.makedirs(output_path, exist_ok=True)
+    if clean_up:
+        _cleanup_output(output_path=output_path)
+    _orbit_system_commons(context=context, output_path=output_path)
 
     if context.account_id is None:
         raise ValueError("context.account_id is None!")
@@ -194,11 +206,21 @@ def _generate_env_manifest(context: "Context", clean_up: bool = True) -> str:
     if context.identity_pool_id is None:
         raise ValueError("context.identity_pool_id is None!")
 
-    _k8_dashboard(output_path=output_path, context=context)
-    _cluster_autoscaler(output_path=output_path, context=context)
-
     if context.install_ssm_agent:
         _ssm_agent_installer(output_path=output_path, context=context)
+
+    return output_path
+
+
+def _generate_env_manifest(context: "Context", clean_up: bool = True) -> str:
+    output_path = os.path.join(".orbit.out", context.name, "kubectl", "env")
+    os.makedirs(output_path, exist_ok=True)
+    if clean_up:
+        _cleanup_output(output_path=output_path)
+
+    input = os.path.join(MODELS_PATH, "env", "commons.yaml")
+    output = os.path.join(output_path, "commons.yaml")
+    shutil.copyfile(src=input, dst=output)
 
     return output_path
 
@@ -208,7 +230,7 @@ def _prepare_team_context_path(context: "Context") -> str:
     os.makedirs(output_path, exist_ok=True)
     _cleanup_output(output_path=output_path)
     if context.account_id is None:
-        raise RuntimeError("context.account_id is None!")
+        raise ValueError("context.account_id is None!")
     return output_path
 
 
@@ -270,7 +292,7 @@ def _efs_driver_base(output_path: str) -> None:
 
 
 def _generate_efs_driver_manifest(context: "Context") -> str:
-    output_path = os.path.join(".orbit.out", context.name, "kubectl", "efs_driver")
+    output_path = os.path.join(".orbit.out", context.name, "kubectl", "kube-system", "efs_driver")
     os.makedirs(output_path, exist_ok=True)
     _cleanup_output(output_path=output_path)
     if context.account_id is None:
@@ -308,7 +330,7 @@ def _fsx_driver_base(output_path: str, context: "Context") -> None:
 
 
 def _generate_fsx_driver_manifest(context: "Context") -> str:
-    output_path = os.path.join(".orbit.out", context.name, "kubectl", "fsx_driver")
+    output_path = os.path.join(".orbit.out", context.name, "kubectl", "kube-system", "fsx_driver")
     os.makedirs(output_path, exist_ok=True)
     _cleanup_output(output_path=output_path)
     if context.account_id is None:
@@ -337,17 +359,19 @@ def deploy_env(context: "Context") -> None:
         k8s_context = get_k8s_context(context=context)
         _logger.debug("k8s_context: %s", k8s_context)
 
-        # EFS Driver
-        output_path = _generate_efs_driver_manifest(context=context)
-        sh.run(f"kubectl apply -k {output_path} --context {k8s_context} --wait")
+        # kube-system
+        output_path = _generate_kube_system_manifest(context=context)
+        sh.run(f"kubectl apply -f {output_path} --context {k8s_context} --wait")
 
-        # FSX Driver
-        output_path = _generate_fsx_driver_manifest(context=context)
-        sh.run(f"kubectl apply -k {output_path} --context {k8s_context} --wait")
+        # orbit-system
+        output_path = _generate_orbit_system_manifest(context=context)
+        sh.run(f"kubectl apply -f {output_path} --context {k8s_context} --wait")
 
-        # Orbit Env
+        # env
         output_path = _generate_env_manifest(context=context)
         sh.run(f"kubectl apply -f {output_path} --context {k8s_context} --wait")
+
+        # Enable ENIs
         sh.run(f"kubectl set env daemonset aws-node -n kube-system --context {k8s_context} ENABLE_POD_ENI=true")
 
 
@@ -358,7 +382,7 @@ def deploy_team(context: "Context", team_context: "TeamContext") -> None:
         k8s_context = get_k8s_context(context=context)
         _logger.debug("kubectl context: %s", k8s_context)
         output_path = _generate_team_context(context=context, team_context=team_context)
-        output_path = _generate_env_manifest(context=context, clean_up=False)
+        # output_path = _generate_orbit_system_manifest(context=context, clean_up=False)
         sh.run(f"kubectl apply -f {output_path} --context {k8s_context} --wait")
 
 
@@ -369,7 +393,7 @@ def destroy_env(context: "Context") -> None:
         sh.run(f"eksctl utils write-kubeconfig --cluster orbit-{context.name} --set-kubeconfig-context")
         k8s_context = get_k8s_context(context=context)
         _logger.debug("kubectl k8s_context: %s", k8s_context)
-        output_path = _generate_env_manifest(context=context)
+        output_path = _generate_orbit_system_manifest(context=context)
         try:
             sh.run(
                 f"kubectl delete -f {output_path} --grace-period=0 --force "
