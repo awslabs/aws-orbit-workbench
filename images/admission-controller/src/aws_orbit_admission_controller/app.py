b802e0cf5a8a8b4eee7e2ade00992ca776d2ae6f
abaror <22354730+abaror@users.noreply.github.com>
2021-05-10 12:39:47 -0400

    minor fixes to admission app

--- images/admission-controller/src/aws_orbit_admission_controller/app.py
+++ images/admission-controller/src/aws_orbit_admission_controller/app.py
@@ -5,6 +5,7 @@ from typing import Any
 from aws_orbit_admission_controller.namespace import process_request as process_namespace_request
 from aws_orbit_admission_controller.pod import process_request as process_pod_request
 from flask import Flask, request
+import random
 
 app = Flask(__name__)
 app.logger.info("environ: %s", os.environ)
@@ -29,6 +30,10 @@ def health() -> Any:
     app.logger.debug("Health check")
     return ("", http.HTTPStatus.NO_CONTENT)
 
+@app.route('/hello')
+def hello():
+    r = random.randint(0, 1000)
+    return f'Hello! random number gen: {r}'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)  # pragma: no cover
+    app.run(host="0.0.0.0", debug=True, use_reloader=True)  # pragma: no cover
