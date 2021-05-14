051e0b7fd404a2b566b7849e7652db4e27d9346c
Derek Graeber <dgraeber@amazon.com>
2021-05-14 13:37:50 -0400

    Initial commit of sagemaker-operator plugin (#482)
    * Initial commit of sagemaker-operator plugin
    
    * Needed to parameterize the region on the job
    
    * Updating - cluster roles and crd, labels
    
    * parameterize notebook
    
    * Changing name of notebook to be intuitive
    
    * adding labels
    
    * adding 2 blanks lines to pass static checks

--- cli/aws_orbit/remote_files/kubectl.py
+++ cli/aws_orbit/remote_files/kubectl.py
@@ -104,6 +104,13 @@ def _ssm_agent_installer(output_path: str, context: "Context") -> None:
     shutil.copyfile(src=input, dst=output)
 
 
+def _sm_operator_installer(output_path: str, context: "Context") -> None:
+    filename = "10-sm-operator.yaml"
+    input = os.path.join(MODELS_PATH, "apps", filename)
+    output = os.path.join(output_path, filename)
+    shutil.copyfile(src=input, dst=output)
+
+
 def _team(context: "Context", team_context: "TeamContext", output_path: str) -> None:
     input = os.path.join(MODELS_PATH, "apps", "01-team.yaml")
     output = os.path.join(output_path, f"01-{team_context.name}-team.yaml")
@@ -199,6 +206,8 @@ def _generate_env_manifest(context: "Context", clean_up: bool = True) -> str:
 
     if context.install_ssm_agent:
         _ssm_agent_installer(output_path=output_path, context=context)
+
+    _sm_operator_installer(output_path=output_path, context=context)
 
     return output_path
 
