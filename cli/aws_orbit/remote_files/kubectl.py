02fb3f55bddca8aed019df81278a38bc070b6546
Derek Graeber <dgraeber@amazon.com>
2021-05-14 13:09:56 -0400

    adding 2 blanks lines to pass static checks

--- cli/aws_orbit/remote_files/kubectl.py
+++ cli/aws_orbit/remote_files/kubectl.py
@@ -103,6 +103,7 @@ def _ssm_agent_installer(output_path: str, context: "Context") -> None:
     output = os.path.join(output_path, filename)
     shutil.copyfile(src=input, dst=output)
 
+
 def _sm_operator_installer(output_path: str, context: "Context") -> None:
     filename = "10-sm-operator.yaml"
     input = os.path.join(MODELS_PATH, "apps", filename)
