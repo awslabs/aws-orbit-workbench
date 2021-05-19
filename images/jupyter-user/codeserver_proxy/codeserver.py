b1a8d05f5b579b9da6c5c2f99de0f89aee505140
Derek Graeber <dgraeber@amazon.com>
2021-05-19 17:22:35 -0400

    Static Check - Thrice....and I give in...

--- images/jupyter-user/codeserver_proxy/codeserver.py
+++ images/jupyter-user/codeserver_proxy/codeserver.py
@@ -1,5 +1,4 @@
 def setup_codeserver():
-
     def _command(port):
         return ["bash", "/opt/orbit/codeserver_proxy/startup.sh", "{port}"]
 
@@ -7,8 +6,5 @@ def setup_codeserver():
         "command": _command,
         "timeout": 120,
         "absolute_url": False,
-        "launcher_entry": {
-            "icon_path": "/opt/orbit/codeserver_proxy/favicon.svg",
-            "title": "VS Code"
-        }
+        "launcher_entry": {"icon_path": "/opt/orbit/codeserver_proxy/favicon.svg", "title": "VS Code"},
     }
--- images/jupyter-user/codeserver_proxy/getcreds.py
+++ images/jupyter-user/codeserver_proxy/getcreds.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
+import fnmatch
 import json
 import os
-import fnmatch
 
-
-aws_dir = '/home/jovyan/.aws/'
-cache_dir = aws_dir + 'cli/cache'
-creds = aws_dir + 'credentials'
+aws_dir = "/home/jovyan/.aws/"
+cache_dir = aws_dir + "cli/cache"
+creds = aws_dir + "credentials"
 fN = None
 
 os.system("aws s3 ls")
 
 for file in os.listdir(cache_dir):
-    if fnmatch.fnmatch(file, '*.json'):
+    if fnmatch.fnmatch(file, "*.json"):
         fN = file
         break
 
@@ -21,9 +20,9 @@ with open(cache_dir + "/" + fN) as f:
     data = json.load(f)
 
 
-with open(creds, 'w') as out:
-    l1 = '[default]'
-    l2 = 'aws_access_key_id = ' + data['Credentials']['AccessKeyId']
-    l3 = 'aws_secret_access_key = ' + data['Credentials']['SecretAccessKey']
-    l4 = 'aws_session_token = ' + data['Credentials']['SessionToken']
-    out.write('{}\n{}\n{}\n{}\n'.format(l1, l2, l3, l4))
+with open(creds, "w") as out:
+    l1 = "[default]"
+    l2 = "aws_access_key_id = " + data["Credentials"]["AccessKeyId"]
+    l3 = "aws_secret_access_key = " + data["Credentials"]["SecretAccessKey"]
+    l4 = "aws_session_token = " + data["Credentials"]["SessionToken"]
+    out.write("{}\n{}\n{}\n{}\n".format(l1, l2, l3, l4))
