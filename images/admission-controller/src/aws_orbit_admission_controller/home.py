a9bbd06b404f526719a7d795aaf3625bc42210cc
abaror <22354730+abaror@users.noreply.github.com>
2021-05-26 21:20:41 -0400

    able to get the user name and email

--- images/admission-controller/src/aws_orbit_admission_controller/home.py
+++ images/admission-controller/src/aws_orbit_admission_controller/home.py
@@ -17,14 +17,37 @@ from typing import Any, Dict, Optional, cast
 from flask import Flask, render_template,request
 import logging
 import json
+import jwt
+import requests
+import base64
+import os
 
 def login(logger: logging.Logger, app: Flask) -> Any:
     logger.debug("cookies: %s", json.dumps(request.cookies))
     return render_template('index.html', title='login')
 
 def logout(logger: logging.Logger, app: Flask) -> Any:
-    logger.debug("cookies: %s", json.dumps(request.cookies))
     logger.debug("headers: %s", json.dumps(dict(request.headers)))
-    logger.debug("authorization: %s", json.dumps(request.authorization))
+
+    encoded_jwt = request.headers['x-amzn-oidc-data']
+    jwt_headers = encoded_jwt.split('.')[0]
+    decoded_jwt_headers = base64.b64decode(jwt_headers)
+    decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
+    decoded_json = json.loads(decoded_jwt_headers)
+    kid = decoded_json['kid']
+    region = os.environ['AWS_REGION']
+
+    # Step 2: Get the public key from regional endpoint
+    url = 'https://public-keys.auth.elb.' + region + '.amazonaws.com/' + kid
+    req = requests.get(url)
+    pub_key = req.text
+
+    # Step 3: Get the payload
+    payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES256'])
+    logger.debug("payload:\n %s",payload)
+    username = payload['username']
+    email = payload['email']
+    logger.debug("username: %s, email: email", payload)
+
 
     return render_template('index.html', title='logout')
