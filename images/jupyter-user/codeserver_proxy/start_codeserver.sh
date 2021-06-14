606aff9976dd7c2149b0bb04e36796fddbb4c0a5
Derek Graeber <dgraeber@amazon.com>
2021-06-14 18:22:16 -0400

    manually install addiditonal driver on first start of vscode (#658)
    Co-authored-by: AMIR BAR-OR <22354730+abaror@users.noreply.github.com>

--- images/jupyter-user/codeserver_proxy/start_codeserver.sh
+++ images/jupyter-user/codeserver_proxy/start_codeserver.sh
@@ -5,6 +5,24 @@ port=$1
 echo $port
 export PATH="/opt/orbit/apps/codeserver/bin:$PATH"
 
+DRIVER_DIR="/home/jovyan/shared/drivers"
+INSTALL_FLAG="/home/jovyan/private/code-server/INSTALLED_DRIVERS"
+if [ -d "${DRIVER_DIR}" ] && ! [ -f "${INSTALL_FLAG}" ];then
+    for driver in `ls "${DRIVER_DIR}" | grep .vsix`
+    do
+        echo "Installing ${driver}"
+        echo $driver
+        code-server --install-extension ${DRIVER_DIR}/${driver} \
+        --extensions-dir /home/jovyan/private/code-server/extensions --force \
+        --user-data-dir /home/jovyan/private/code-server \
+        --config /home/jovyan/private/code-server/.config/code-server/config.yaml
+    done
+    touch "${INSTALL_FLAG}"
+else
+    echo "Drivers previously installed"
+fi
+
+# Now fire up VSCode
 code-server --bind-addr localhost:$port --auth none \
 --user-data-dir /home/jovyan/private/code-server \
 --config /home/jovyan/private/code-server/.config/code-server/config.yaml
