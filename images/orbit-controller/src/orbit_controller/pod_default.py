ae9e9778eae434fa2a5ec2fa2b472bb53b2bbe76
Chauncy McCaughey <chamcca@amazon.com>
2021-06-24 15:09:02 -0600

    use strings for resource_version check

--- images/orbit-controller/src/orbit_controller/pod_default.py
+++ images/orbit-controller/src/orbit_controller/pod_default.py
@@ -162,7 +162,7 @@ def watch(queue: Queue, state: Dict[str, Any]) -> int:  # type: ignore
                 "label_selector": "orbit/space=team",
             }
             resource_version = state.get("lastResourceVersion", 0)
-            if resource_version >= 0:
+            if resource_version != "-1":
                 kwargs["resource_version"] = resource_version
 
             for event in api.watch(**kwargs):
@@ -187,7 +187,7 @@ def watch(queue: Queue, state: Dict[str, Any]) -> int:  # type: ignore
         except k8s_exceptions.ApiException as ae:
             if ae.reason.startswith("Expired: too old resource version"):
                 logger.warning(ae.reason)
-                state["lastResourceVersion"] = -1
+                state["lastResourceVersion"] = "-1"
             else:
                 logger.exception("Unknown ApiException in PodDefaultWatcher. Failing")
                 raise
