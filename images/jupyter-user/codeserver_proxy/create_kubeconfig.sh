a4c7e1187bcb5bc530384097edd7f10aba4845eb
Derek Graeber <dgraeber@amazon.com>
2021-07-13 19:35:45 -0400

    removed cell outputs and local account info

--- images/jupyter-user/codeserver_proxy/create_kubeconfig.sh
+++ images/jupyter-user/codeserver_proxy/create_kubeconfig.sh
@@ -4,21 +4,21 @@ env_param='AWS_ORBIT_ENV='
 ep=$(env | grep $env_param)
 cluster_name=orbit-${ep/$env_param/}
 echo $cluster_name
-#orbit-env-r110e-dev
+
 
 user_param='AWS_ORBIT_TEAM_SPACE='
 up=$(env | grep $user_param)
 team_name=${up/$user_param/}
 echo $team_name
-#lake-user
+
 
 server=$(aws eks describe-cluster --name $cluster_name --query 'cluster.endpoint' | sed "s/\"//g")
 echo $server
-#https://49B3DA69C6B18379E92C086697CB43CB.gr7.us-west-2.eks.amazonaws.com
+
 
 name=$(kubectl get secret -oname | grep default-editor-token)
 echo $name
-#secret/default-editor-token-l4v9f
+
 
 ca=$(kubectl get $name -o jsonpath='{.data.ca\.crt}')
 token=$(kubectl get $name -o jsonpath='{.data.token}' | base64 --decode)
