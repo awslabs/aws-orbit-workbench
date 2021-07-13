544af9e4926984722e59a3a982b510084afeef6c
Derek Graeber <dgraeber@amazon.com>
2021-07-13 16:51:38 -0400

    Adding smlog capability and fixing kube credentials

--- images/jupyter-user/codeserver_proxy/create_kubeconfig.sh
+++ images/jupyter-user/codeserver_proxy/create_kubeconfig.sh
@@ -3,30 +3,31 @@
 env_param='AWS_ORBIT_ENV='
 ep=$(env | grep $env_param)
 cluster_name=orbit-${ep/$env_param/}
+echo $cluster_name
+#orbit-env-r110e-dev
 
 user_param='AWS_ORBIT_TEAM_SPACE='
 up=$(env | grep $user_param)
 team_name=${up/$user_param/}
+echo $team_name
+#lake-user
 
-#server=$(aws eks describe-cluster --name $cluster_name --query 'cluster.endpoint' | sed "s/\"//g")
-server=""
-name=$(kubectl get secret -oname | grep $team_name-token)
+server=$(aws eks describe-cluster --name $cluster_name --query 'cluster.endpoint' | sed "s/\"//g")
+echo $server
+#https://49B3DA69C6B18379E92C086697CB43CB.gr7.us-west-2.eks.amazonaws.com
+
+name=$(kubectl get secret -oname | grep default-editor-token)
+echo $name
+#secret/default-editor-token-l4v9f
 
 ca=$(kubectl get $name -o jsonpath='{.data.ca\.crt}')
 token=$(kubectl get $name -o jsonpath='{.data.token}' | base64 --decode)
 namespace=$(kubectl get $name -o jsonpath='{.data.namespace}' | base64 --decode)
 
-#echo $server
-#echo $name
-#echo $ca
-#echo $token
-#echo $namespace
-#echo $team_name
-#echo $cluster_name
+echo $ca
+echo $token
+echo $namespace
 
-if ! [ -d "/home/jovyan/.kube" ]; then
-  mkdir /home/jovyan/.kube
-fi
 
 echo "
 apiVersion: v1
@@ -48,6 +49,3 @@ users:
   user:
     token: ${token}
 " > /home/jovyan/.kube/config
-
-#mkdir /home/jovyan/.kube -p
-#cp sa.kubeconfig /home/jovyan/.kube/config
