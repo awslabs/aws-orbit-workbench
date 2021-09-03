7eb5283a8c4b1e9f856968b1a5f19e6a687137ed
Derek Graeber <dgraeber@amazon.com>
2021-09-03 12:52:30 -0400

    fixing orbit-controller helm update, increasing resources on orbit-runners

--- images/orbit-controller/src/orbit_controller/namespace.py
+++ images/orbit-controller/src/orbit_controller/namespace.py
@@ -134,6 +134,13 @@ def process_added_event(namespace: Dict[str, Any]) -> None:
     repo = f"{team}--userspace"
     # add the team repo
     run_command(f"helm repo add {repo} {helm_repo_url}")
+    
+    try:
+        # In isolated envs, we cannot refresh stable, and since we don't use it, we remove it
+        run_command("helm repo remove stable")
+    except:
+        logger.info("Tried to remove stable repo...got an error, but moving on")
+    
     run_command("helm repo update")
     run_command(f"helm search repo --devel {repo} -o json > /tmp/charts.json")
     with open("/tmp/charts.json", "r") as f:
