d1f07b7d2098939f8bdbc997bf80e904482dd1dd
Ramon Bautista <bautramo@amazon.com>
2021-10-01 10:30:22 -0400

    specify test file

--- test/regressions/scripts/pytest-lake-user.sh
+++ test/regressions/scripts/pytest-lake-user.sh
@@ -3,10 +3,10 @@
 #set -e
 
 # Set the .kube/config with respect to runtime environment
-pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml
+pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml test_lake_user.py
 
 cat .pytest_cache/v/cache/lastfailed
 
 if [[ $? -ne 0 ]]; then
-    pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml --lf
+    pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml --lf test_lake_user.py
 fi
