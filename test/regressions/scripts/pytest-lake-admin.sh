59794ccc813e5d08539345b032e35904191f092f
Ramon Bautista <bautramo@amazon.com>
2021-10-01 16:41:05 -0400

    Moved the pytest options to config

--- test/regressions/scripts/pytest-lake-admin.sh
+++ test/regressions/scripts/pytest-lake-admin.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# set -e
+set -e
+# Required os env variables. Replace with testing Orbit env details
+# AWS_ORBIT_ENV, AWS_ORBIT_TEAM_SPACE
 
 # Set the .kube/config with respect to runtime environment
-pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeadmin -n auto --junitxml=regression_report.xml
+pytest -k testlakeadmin -n auto --junitxml=regression_report.xml
 
 cat .pytest_cache/v/cache/lastfailed
 
 if [[ $? -ne 0 ]]; then
-    pytest -c ./pytest.ini --kube-config ~/.kube/config -v -s -k testlakeadmin -n auto --junitxml=regression_report.xml --lf
+    pytest -k testlakeadmin -n auto --junitxml=regression_report.xml --lf
 fi
--- test/regressions/scripts/pytest-lake-user.sh
+++ test/regressions/scripts/pytest-lake-user.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#set -e
+set -e
+# Required os env variables. Replace with testing Orbit env details
+# AWS_ORBIT_ENV, AWS_ORBIT_TEAM_SPACE
 
-# Set the .kube/config with respect to runtime environment
-export AWS_ORBIT_ENV='iter'
-export AWS_ORBIT_TEAM_SPACE='lake-user'
-pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml test_lake_user.py
+pytest -k testlakeuser -n auto --junitxml=regression_report.xml test_lake_user.py
 
 cat .pytest_cache/v/cache/lastfailed
 
 if [[ $? -ne 0 ]]; then
-    pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml --lf test_lake_user.py
+    pytest -k testlakeuser -n auto --junitxml=regression_report.xml --lf test_lake_user.py
 fi
