424268fd55b2e39d6e7de51379bde444b0850245
Ramon Bautista <bautramo@amazon.com>
2021-10-01 13:25:38 -0400

    Added env var

--- test/regressions/scripts/pytest-lake-user.sh
+++ test/regressions/scripts/pytest-lake-user.sh
@@ -3,6 +3,8 @@
 #set -e
 
 # Set the .kube/config with respect to runtime environment
+AWS_ORBIT_ENV=${ENV_NAME}
+AWS_ORBIT_TEAM_SPACE=${TEST_TEAM_SPACE}
 pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeuser -n auto --junitxml=regression_report.xml test_lake_user.py
 
 cat .pytest_cache/v/cache/lastfailed
