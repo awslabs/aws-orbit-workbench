3165309e13b77f2ef3311a75170dacb25ed48c20
Ramon Bautista <bautramo@amazon.com>
2021-10-06 13:08:12 -0400

    specify config

--- test/regressions/scripts/pytest-lake-admin.sh
+++ test/regressions/scripts/pytest-lake-admin.sh
@@ -6,12 +6,10 @@
 # AWS_ORBIT_ENV, AWS_ORBIT_TEAM_SPACE
 
 # Set the .kube/config with respect to runtime environment
-pytest --rootdir . --kube-config ~/.kube/config -v -s  -k testlakeadmin -n auto --junitxml=regression_report.xml
+pytest --rootdir . -c ./pytest.ini --kube-config ~/.kube/config -v -s  -k testlakeadmin -n auto --junitxml=regression_report.xml
 
-if [[ $? -eq 0 ]]; then
+cat .pytest_cache/v/cache/lastfailed
+
+if [[ $? -ne 0 ]]; then
     pytest -c ./pytest.ini --kube-config ~/.kube/config -v -s -k testlakeadmin -n auto --junitxml=regression_report.xml --lf
 fi
-
-ls -lha
-ls -lha .pytest_cache/v/cache
-cat .pytest_cache/v/cache/lastfailed
