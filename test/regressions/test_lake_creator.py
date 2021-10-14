1ac4562bb408b449a508469d941bf62b2563aaa6
Ramon Bautista <36557237+rb201@users.noreply.github.com>
2021-10-14 12:29:57 -0400

    Reduced timeouts to allow faster tretrying of testing (#1125)

--- test/regressions/test_lake_creator.py
+++ test/regressions/test_lake_creator.py
@@ -117,7 +117,7 @@ def test_lakecreator_extractor(zip_file, kube: TestClient):
     # Logic to wait till OrbitJob creates
     lakecreator.wait_until_ready(timeout=60)
     # Logic to pass or fail the pytest
-    lakecreator.wait_until_job_completes(timeout=7200)
+    lakecreator.wait_until_job_completes(timeout=600)
     current_status = lakecreator.get_status().get("orbitJobOperator").get("jobStatus")
     logger.info(f"current_status={current_status}")
     # Cleanup
@@ -212,7 +212,7 @@ def test_lakecreator_glue_table_creator(datafile, kube: TestClient):
     # Logic to wait till OrbitJob creates
     lakecreator.wait_until_ready(timeout=60)
     # Logic to pass or fail the pytest
-    lakecreator.wait_until_job_completes(timeout=7200)
+    lakecreator.wait_until_job_completes(timeout=1200)
     current_status = lakecreator.get_status().get("orbitJobOperator").get("jobStatus")
     logger.info(f"current_status={current_status}")
     # Cleanup
@@ -274,7 +274,7 @@ def test_lakecreator_lf(kube: TestClient):
     # Logic to wait till OrbitJob creates
     lakecreator.wait_until_ready(timeout=60)
     # Logic to pass or fail the pytest
-    lakecreator.wait_until_job_completes(timeout=7200)
+    lakecreator.wait_until_job_completes(timeout=1800)
     current_status = lakecreator.get_status().get("orbitJobOperator").get("jobStatus")
     logger.info(f"current_status={current_status}")
     # Cleanup
