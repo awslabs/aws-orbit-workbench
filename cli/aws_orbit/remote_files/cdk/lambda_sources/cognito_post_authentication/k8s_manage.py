f08cab9d686f6ba49f4f031b3f8acebfce80a6cf
stthoom <stthoom@amazon.com>
2021-10-18 17:01:16 -0400

    Refactored post authentication handler k8s dynamic client

--- cli/aws_orbit/remote_files/cdk/lambda_sources/cognito_post_authentication/k8s_manage.py
+++ cli/aws_orbit/remote_files/cdk/lambda_sources/cognito_post_authentication/k8s_manage.py
@@ -25,12 +25,9 @@ ORBIT_STATE_PATH = os.environ.get("ORBIT_STATE_PATH", "/state")
 USERSPACE_CR_KIND = "UserSpace"
 KUBECONFIG_PATH = "/tmp/.kubeconfig"
 
-USERSPACE_DYNAMIC_CIENT = dynamic.DynamicClient(client=api_client.ApiClient()).resources.get(
-        group=ORBIT_API_GROUP, api_version=ORBIT_API_VERSION, kind=USERSPACE_CR_KIND
-    )
+
 
 ssm = boto3.client("ssm")
-
 context = ssm.get_parameter(Name=f"/orbit/{ORBIT_ENV}/context")
 EFS_FS_ID = json.loads(context.get("Parameter").get("Value")).get("SharedEfsFsId")
 
@@ -39,8 +36,11 @@ def handler(event: Dict[str, Any], context: Optional[Dict[str, Any]]) -> Any:
     create_kubeconfig()
 
     api_CoreV1 = client.CoreV1Api()
+    userspace_dc = dynamic.DynamicClient(client=api_client.ApiClient()).resources.get(
+        group=ORBIT_API_GROUP, api_version=ORBIT_API_VERSION, kind=USERSPACE_CR_KIND
+    )
 
-    manage_user_namespace(event, api_CoreV1)
+    manage_user_namespace(event, api_CoreV1, userspace_dc)
 
 
 def run_command(cmd: str) -> None:
@@ -90,14 +90,8 @@ def create_user_efs_endpoint(user: str, team_name: str) -> Dict[str, Any]:
     )
 
 
-def get_dynamic_client(cr_kind: str):
-    api_dc = dynamic.DynamicClient(client=api_client.ApiClient()).resources.get(
-        group=ORBIT_API_GROUP, api_version=ORBIT_API_VERSION, kind=cr_kind
-    )
-    return api_dc
-
-
-def create_cr(
+def create_userspace(
+    userspace_dc: dynamic.DynamicClient,
     cr_kind: str,
     name: str,
     env: str,
@@ -131,13 +125,14 @@ def create_cr(
     if owner_reference is not None:
         userspace["metadata"]["ownerReferences"] = [owner_reference]
 
-    userspace_cr = USERSPACE_DYNAMIC_CIENT.create(namespace=name, body=userspace).to_dict()
+    userspace_cr = userspace_dc.create(namespace=name, body=userspace).to_dict()
 
     return userspace_cr
 
 
 def create_user_namespace(
     api: client.CoreV1Api,
+    userspace_dc: dynamic.DynamicClient,
     user_name: str,
     user_email: str,
     expected_user_namespaces: Dict[str, str],
@@ -188,7 +183,8 @@ def create_user_namespace(
 
             try:
                 # create userspace custom resource for the given user namespace
-                create_cr(
+                create_userspace(
+                    userspace_dc=dynamic.DynamicClient,
                     name=user_ns,
                     env=os.environ.get("ORBIT_ENV"),
                     space="user",
@@ -227,7 +223,11 @@ def delete_user_efs_endpoint(user_name: str, user_namespace: str, api: client.Co
 
 
 def delete_user_namespace(
-    api: client.CoreV1Api, user_name: str, expected_user_namespaces: Dict[str, str], namespaces: List[str]
+    api: client.CoreV1Api,
+    userspace_dc: dynamic.DynamicClient,
+    user_name: str,
+    expected_user_namespaces: Dict[str, str],
+    namespaces: List[str]
 ) -> None:
     for user_ns in namespaces:
         if user_ns not in expected_user_namespaces.values():
@@ -237,7 +237,7 @@ def delete_user_namespace(
 
             logger.info(f"User {user_name} is not expected to be part of the {user_ns} namespace. Removing...")
             try:
-                USERSPACE_DYNAMIC_CIENT.delete(name=user_ns, namespace=user_ns)
+                userspace_dc.delete(name=user_ns, namespace=user_ns)
                 logger.info(f"Removed userspace custom resource {user_ns}")
             except Exception as ae:
                 logger.error(f"Exception when trying to remove userspace custom resource {user_ns}")
@@ -259,7 +259,7 @@ def delete_user_profile(user_profile: str) -> None:
     time.sleep(5)
 
 
-def manage_user_namespace(event: Dict[str, Any], api: client.CoreV1Api) -> None:
+def manage_user_namespace(event: Dict[str, Any], api: client.CoreV1Api, userspace_dc: dynamic.DynamicClient) -> None:
     user_name = cast(str, event.get("user_name"))
     user_email = cast(str, event.get("user_email"))
     expected_user_namespaces = cast(Dict[str, str], event.get("expected_user_namespaces"))
@@ -273,6 +273,7 @@ def manage_user_namespace(event: Dict[str, Any], api: client.CoreV1Api) -> None:
 
     create_user_namespace(
         api=api,
+        userspace_dc=userspace_dc,
         user_name=user_name,
         user_email=user_email,
         expected_user_namespaces=expected_user_namespaces,
@@ -281,6 +282,7 @@ def manage_user_namespace(event: Dict[str, Any], api: client.CoreV1Api) -> None:
 
     delete_user_namespace(
         api=api,
+        userspace_dc=userspace_dc,
         user_name=user_name,
         expected_user_namespaces=expected_user_namespaces,
         namespaces=all_ns,
--- test/regressions/test_userspace.py
+++ test/regressions/test_userspace.py
@@ -13,17 +13,33 @@
 #    limitations under the License.
 
 import os
-
+import boto3
+import json
 import pytest
+import logging
+from typing import Any, Dict, List, Optional, cast
 from custom_resources import CustomApiObject
+from kubernetes import client
 from kubetest.client import TestClient
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger()
 
 MANIFESTS_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "manifests",
 )
+ORBIT_ENV="iter"
+TEAM_NAME = "lake-admin"
 
+USER_NAME = "orbitpytest"
+USER_EMAIL = F"{USER_NAME}@AMAZON.COM"
+USER_NS = "lake-admin-orbitpytest"
 
 class UserSpace(CustomApiObject):
     group = "orbit.aws"
@@ -35,26 +51,54 @@ class UserSpace(CustomApiObject):
         return self.obj.get("status", {}).get("installation", {}).get("installationStatus") == "Installed"
 
 
+def create_user_efs_endpoint(user: str, team_name: str) -> Dict[str, Any]:
+    ssm = boto3.client("ssm")
+    env_context = ssm.get_parameter(Name=f"/orbit/{ORBIT_ENV}/context")
+    EFS_FS_ID = json.loads(env_context.get("Parameter").get("Value")).get("SharedEfsFsId")
+    efs = boto3.client("efs")
+    return cast(
+        Dict[str, str],
+        efs.create_access_point(
+            FileSystemId=EFS_FS_ID,
+            PosixUser={"Uid": 1000, "Gid": 100},
+            RootDirectory={
+                "Path": f"/{team_name}/private/{user}",
+                "CreationInfo": {"OwnerUid": 1000, "OwnerGid": 100, "Permissions": "770"},
+            },
+            Tags=[{"Key": "TeamSpace", "Value": team_name}, {"Key": "Env", "Value": ORBIT_ENV}],
+        ),
+    )
+
+
 @pytest.mark.order(1)
-@pytest.mark.namespace(create=False)
-def test_userspace(kube: TestClient) -> None:
+@pytest.mark.namespace(create=True, name=USER_NS)
+@pytest.mark.testuserspace_cr
+def test_userspace_creation(kube: TestClient) -> None:
+    logger.info(f"Creating EFS endpoint for {USER_NAME}...")
+    resp = create_user_efs_endpoint(user=USER_NAME, team_name=TEAM_NAME)
+    access_point_id = resp.get("AccessPointId")
+
     body = {
         "apiVersion": "orbit.aws/v1",
         "kind": "UserSpace",
         "metadata": {
-            "name": "user-space-test",
+            "name": USER_NS,
+            "namespace": USER_NS,
         },
         "spec": {
-            "env": "dev-env",
+            "env": ORBIT_ENV,
             "space": "user",
-            "team": "test-team",
-            "user": "test-user",
-            "userEfsApId": "test-userEfsApId",
-            "userEmail": "test-userEmail",
+            "team": TEAM_NAME,
+            "user": USER_NAME,
+            "userEfsApId": access_point_id,
+            "userEmail": USER_EMAIL
         },
     }
-
+    # Create UserSpace custom resource
     userspace = UserSpace(body)
-    userspace.create(namespace="orbit-system")
+    userspace.create(namespace=TEAM_NAME)
+    # Wait for creation
     userspace.wait_until_ready(timeout=30)
+    # Check the Helm chart installation status
+    # Delete the UserSpace custom resource
     userspace.delete()
