0abe71eedbbfe10d64107230270e2e1b0ef25bc1
Ramon Bautista <36557237+rb201@users.noreply.github.com>
2021-10-19 17:40:16 -0400

    rerun pytest if there was a previous failure (#1134)
    * Only rerun pytest if something had failed; added a marker for lakecreator
    
    * Rerun if previous failure
    
    * Skip lakeformation related tests for iso env

--- test/regressions/scripts/pytest-lake-admin.sh
+++ test/regressions/scripts/pytest-lake-admin.sh
@@ -5,16 +5,10 @@ set -e
 # AWS_ORBIT_ENV, AWS_ORBIT_TEAM_SPACE
 
 # Set the .kube/config with respect to runtime environment
-if [[ "${ENV_NAME}" == *"-iso"* ]]; then
-    pytest -k "testlakeadmin and not testlakeadmin_noniso" -n auto --junitxml=regression_report.xml test_lake_admin.py
 
-    cat .pytest_cache/v/cache/lastfailed || echo "No failed test"
+pytest -k testlakeadmin -n auto --junitxml=regression_report.xml test_lake_admin.py
 
-    pytest -k "testlakeadmin and not testlakeadmin_noniso" -n auto --junitxml=regression_report_last_failed.xml test_lake_admin.py
-else
-    pytest -k testlakeadmin -n auto --junitxml=regression_report.xml test_lake_admin.py
-
-    cat .pytest_cache/v/cache/lastfailed || echo "No failed test"
-
+if cat .pytest_cache/v/cache/lastfailed > /dev/null 2>&1; then
+    echo -e "\nRetrying the last failed tests\n"
     pytest -k testlakeadmin -n auto --junitxml=regression_report_last_failed.xml test_lake_admin.py
 fi
--- test/regressions/test_lake_creator.py
+++ test/regressions/test_lake_creator.py
@@ -243,6 +243,7 @@ def test_lakecreator_glue_tables(kube: TestClient):
 @pytest.mark.order(5)
 @pytest.mark.namespace(create=False)
 @pytest.mark.testlakecreator_lf
+@pytest.mark.skipif("iso" in workspace.get("env_name"), reason="lakeformation endpoint is unreachable in iso env")
 def test_lakecreator_lf(kube: TestClient):
     notebook_to_run = {
         "apiVersion": "orbit.aws/v1",
