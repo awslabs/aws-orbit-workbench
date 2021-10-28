de60b11f794276b6618cf7dc730b5bfea470d824
stthoom <stthoom@amazon.com>
2021-10-28 14:53:42 -0400

    IAM role length validator changes

--- cli/aws_orbit/models/manifest.py
+++ cli/aws_orbit/models/manifest.py
@@ -440,17 +440,18 @@ def manifest_validations(manifest: Manifest) -> None:
     region_len = len(utils.get_region())
     env_len = len(manifest.name)
     env_region_len = env_len + region_len
+    print(f"env_region_len={env_region_len}")
     env_region_max = 28
     env_region_team_max = 51
-    if env_region_len <= env_region_max:
+    if env_region_len > env_region_max:
         raise ValueError(f"Orbit accepts max combined length of {env_region_max} characters for env and region")
 
     max_team_len = max([len(team.name) for team in manifest.teams])
     env_region_team_len = env_region_len + max_team_len
-    if env_region_team_len <= env_region_team_max:
+    print(f"env_region_team_len={env_region_team_len}")
+    if env_region_team_len > env_region_team_max:
         raise ValueError(
             f"Orbit accepts max combined length of {env_region_team_max} characters for env, "
-            f"region, and team name(s)"
+            f"region, and team name"
         )
 
-    raise ValueError("Testing")
